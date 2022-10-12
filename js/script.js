932d1fff8346a55fecb1e398b697fe513b00d2be
Karina Penkala <marsz@poczta.onet.pl>
2022-10-12 11:25:58 +0200

    add result

--- js/script.js
+++ js/script.js
@@ -24,8 +24,7 @@ if(randomNumber == 1){
   computerMove = 'kamień';
 } else if(randomNumber == 2){
     computerMove = 'papier';
-}
-else if(randomNumber == 3){
+} else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 
@@ -33,30 +32,31 @@ printMessage('Mój ruch to: ' + computerMove);
 
 //wynik
 
-if(computerMove == 'kamień' && playerMove == 'papier');{
-    printMessage ('Wygrywasz')
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Wygrywasz');
 }
-else if(computerMove == 'nożyce' && playerMove == 'kamień');{
-    printMessage ('Wygrywasz')
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Wygrywasz');
 }
-else if(computerMove == 'papier' && playerMove == 'nożyce');{
-    printMessage ('Wygrywasz')
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Wygrywasz');
 }
-else if(computerMove == 'kamień' && playerMove == 'nożyce');{
-    printMessage ('Przegrywasz')
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz');
 }
-else if(computerMove == 'nożyce' && playerMove == 'papier')||;{
-    printMessage ('Przegrywasz')
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz');
 }
-else if(computerMove == 'papier' && playerMove == 'kamień')||;{
-    printMessage ('Przegrywasz')
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz');
 }
-else if(computerMove == 'kamień' && playerMove == 'kamień')||;{
-    printMessage ('Remis')
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis');
 }
-else if(computerMove == 'papier' && playerMove == 'papier')||;{
-    printMessage ('Remis')
+else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
 }
-else if(computerMove == 'nożyce' && playerMove == 'nożyce')||;{
-    printMessage ('Remis')
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis');
 }
+
